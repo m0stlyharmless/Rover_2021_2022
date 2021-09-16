1eaabcad18a977d5be45409576758ab72e6afd38
Ethan Schaezler <ethan26226@gmail.com>
2021-09-16 08:13:10 +0000

    Add parameter to allow access to display server

--- software/rundocker.sh
+++ software/rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --mount src=$HOME/Github/Rover_2020_2021/,target=/home/rosuser/Github/Rover_2020_2021,type=bind -t -i osumrc
+docker run --net=host -e DISPLAY=:0 --mount src=$HOME/Github/Rover_2020_2021/,target=/home/rover/Github/Rover_2020_2021,type=bind -t -i osumrc
